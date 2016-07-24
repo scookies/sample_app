7b8fa799fa3d297b41bd185b1442e15917c78426
tricial <tricia@owaytrip.com>
2016-07-24 16:51:57 +0630

    edited pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
